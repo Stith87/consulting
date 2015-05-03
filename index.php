2c78a96cd196b7361a7bc6682ce70320c19a8c42
Michael Stith <stith87@gmail.com>
2015-05-03 00:42:09 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,190 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Grow Your Business With The Power of Infusionsoft</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Infusionsoft Consultants</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right">
+            <div class="form-group">
+              <input type="text" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Sign in</button>
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron campaign">
+      <div class="container">
+        <h1>Plan. Implement. Convert.</h1>
+        <h2 class="hidden-xs">I want to...</h2>
+        <div class="col-md-4 col-sm-4 hidden-xs">
+          <div class="circle">
+            <div class="img-container">
+              <img src="img/people.png" style="height: 90px" />
+            </div>
+            <h4>Drive More Traffic</h4>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-4 hidden-xs">         
+          <div class="circle">
+            <div class="img-container">
+              <img src="img/leads.png" style="height: 90px" />
+            </div>
+            <h4>Generate More Leads</h4>
+          </div>
+        </div>
+         <div class="col-md-4 col-sm-4 hidden-xs">
+          <div class="circle">
+            <div class="img-container">
+              <img src="img/money.png" style="height: 90px" />
+            </div>
+            <h4>Increase Conversion</h4>
+          </div>
+        </div>
+            <p class="visible-xs"><a class="btn btn-primary" href="#">Get Started!</a>
+      </div>
+    </div>
+
+    <div class="container-full" id="grow">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-sm-12" >
+          <h1>Harness the Power of Infusionsoft to Multiply Your Sales!</h1>
+          <p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4 details">
+          <h4>Increase Your Traffic, Increase Your Leads</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-4 details">
+          <h4>Automate Your Sales Funnel</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+       </div>
+        <div class="col-md-4 details">
+          <h4>Convert Leads into Brand Loyalists</h4>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+    </div> <!-- /container -->
+    <div class="container-fluid" id="services">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>How We Help</h1>
+          <div class="col-md-4">
+            <div class="circle">
+              <div class="img-container">
+                <img src="img/consulting.png" />
+              </div>
+            </div>
+            <h4>Consulting</h4>            
+          </div>
+           <div class="col-md-4">
+            <div class="circle">
+              <div class="img-container">
+                <img src="img/implement.png" />
+              </div>
+            </div>
+            <h4>Implementation</h4>
+          </div>
+           <div class="col-md-4">
+            <div class="circle">
+              <div class="img-container">
+                <img src="img/code.png" />
+              </div>
+            </div>
+
+            <h4>Development</h4>            
+          </div>
+
+          <a href="#" class="btn btn-success">Contact Us</a>
+        </div>
+      </div>
+
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2" id="offer">
+          <h1>Free Infusionsoft Implementation Checklist</h1>
+          <form class="col-md-8 col-md-offset-2">
+            <div class="form-group">
+              <input type="text" name="first_name" placeholder="First Name" class="form-control" />
+            </div>
+            <div class="form-group">
+              <input type="text" name="email" placeholder="Email" class="form-control" />
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-success" >Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2014</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
